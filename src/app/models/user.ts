5dca089e0fedeebaca22385e396b6e688617b35a
KerenC <46246176+KerenC91@users.noreply.github.com>
2019-04-18 17:39:50 +0300

    First commit

---
+++ src/app/models/user.ts
@@ -0,0 +1,10 @@
+export class User {
+    id: number;
+    email: string;
+    dateOfBirth: string;
+    firstName: string;
+    lastName: string;
+    phoneNumber: number;
+    gender: string;
+    token: string;
+}
